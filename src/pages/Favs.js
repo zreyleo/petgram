519dee1ed6c3e77be93e59ca2e2290af8ddd1235
Regynald Zambrano <zreyleo@gmail.com>
2021-03-24 10:33:39 -0500

    la aplicacion registra los like del usuario

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { FavsWithQuery } from '../container/GetFavorites'
 
 export const Favs = () => {
   return (
-    <h1>Favs</h1>
+    <Fragment>
+      <h1>Favs</h1>
+      <FavsWithQuery />
+    </Fragment>
   )
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,48 +1,48 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-import Context from '../Context'
+import { Context } from '../Context'
 
 export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
+
   return (
-    <Context.Consumer>
-      {
-        ({ isAuth, activateAuth }) => {
-          return (
-            <Fragment>
-              <RegisterMutation>
-                {(register, { data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    register({ variables }).then(activateAuth)
-                  }
+    <Fragment>
+      <RegisterMutation>
+        {(register, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            register({ variables }).then(({ data }) => {
+              const { signup } = data
+              activateAuth(signup)
+            })
+          }
 
-                  const errorMsg = error && 'El usuario ya existe o ocurrio un problema'
+          const errorMsg = error && 'El usuario ya existe o ocurrio un problema'
 
-                  return <UserForm disabled={loading} onSubmit={onSubmit} title="Registrarse" error={errorMsg} />
-                }}
-              </RegisterMutation>
+          return <UserForm disabled={loading} onSubmit={onSubmit} title="Registrarse" error={errorMsg} />
+        }}
+      </RegisterMutation>
 
-              <LoginMutation>
-                {(login, { data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    login({ variables }).then(activateAuth)
-                  }
+      <LoginMutation>
+        {(login, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            login({ variables }).then(({ data }) => {
+              const { login } = data
+              activateAuth(login)
+            })
+          }
 
-                  const errorMsg = error && 'El usuario no existe o password incorrecto'
+          const errorMsg = error && 'El usuario no existe o password incorrecto'
 
-                  return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title="Iniciar Sesi&oacute;n" />
-                }}
-              </LoginMutation>
-            </Fragment>
-          )
-        }
-      }
-    </Context.Consumer>
+          return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title="Iniciar Sesi&oacute;n" />
+        }}
+      </LoginMutation>
+    </Fragment>
   )
 }
